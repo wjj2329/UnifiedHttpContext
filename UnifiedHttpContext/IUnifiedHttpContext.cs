8b21821535e839f086db84d60414a0cac270d5b7
William <wjj2329@gmail.com>
2025-09-02 18:03:22 -0600

    Push files

---
+++ UnifiedHttpContext/IUnifiedHttpContext.cs
@@ -0,0 +1,17 @@
+﻿#if NETFRAMEWORK
+using System.Web;
+#elif NETCOREAPP
+using Microsoft.AspNetCore.Http;
+#endif
+namespace UnifiedHttpContext
+{
+    public interface IUnifiedHttpContext
+    {
+#if NETFRAMEWORK
+        HttpContext GetUnifiedHttpContext();
+#elif NETCOREAPP
+        public HttpContext GetUnifiedHttpContext(IHttpContextAccessor httpContextAccessor);
+#endif
+    }
+}
+    
---
+++ UnifiedHttpContext/UnifiedHttpContext.cs
@@ -0,0 +1,16 @@
+﻿#if NETFRAMEWORK
+using System.Web;
+#elif NETCOREAPP
+using Microsoft.AspNetCore.Http;
+#endif
+namespace UnifiedHttpContext
+{
+    public class UnifiedHttpContext : IUnifiedHttpContext
+    {
+#if NETFRAMEWORK
+        public HttpContext GetUnifiedHttpContext() => HttpContext.Current;
+#elif NETCOREAPP
+        public HttpContext GetUnifiedHttpContext(IHttpContextAccessor httpContextAccessor) => httpContextAccessor.HttpContext;
+#endif
+    }
+}
